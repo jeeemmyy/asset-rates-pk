e06b395ac3b8d4ad0aa49b0c886ab21fc47cc7ae
Zaeem Javed <73387275+jeeemmyy@users.noreply.github.com>
2022-06-08 19:32:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from goldrates import getGoldRates
+from usdrates import getUsdRates
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,11 @@ app = Flask(__name__)
 @app.route('/',methods = ['GET'])
 def hello_gold():
     return getGoldRates()
+
+@app.route('/usd', methods=['GET'])
+def hello_usd():
+    return getUsdRates()
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
